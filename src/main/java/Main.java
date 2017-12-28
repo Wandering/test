8211a07cdb5c646c2673f647eda33cd960a26b7e
杨永平 <wuhun0301@qq.com>
2017-12-28 11:29:55 +0800

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,13 @@ import com.alibaba.fastjson.JSON;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 import org.apache.http.*;
-import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.cookie.BasicClientCookie2;
 
 
 import javax.imageio.ImageIO;
@@ -19,20 +16,33 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.PrimitiveIterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    private static final String regEx = "[ _`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]|\n|\r|\t";
     private static final String simple_host = "ln.122.gov.cn";
     private static final String host = "http://" + simple_host;
+
     public static void main(String[] args) throws URISyntaxException {
+        String hphm1b = "AG98T3";
+        String type = "辽";
+        Integer fdjh = 279903;
+        Map<String, Object> rtnMap = post(hphm1b, type, fdjh);
+        System.out.println(JSON.toJSONString(rtnMap));
+    }
+
+    private static Map<String, Object> post(String hphm1b, String type, Integer fdjh) throws URISyntaxException {
+        Map<String, Object> rtnMap = null;
+        String hphm = type + hphm1b;
+
         CloseableHttpClient closeableHttpClient = HttpClientBuilder
                 .create()
                 .build();
         HttpClientContext clientContext = HttpClientContext.create();
-        HttpGet httpGet = new HttpGet(host+"/captcha?nocache=" + System.currentTimeMillis());
+        HttpGet httpGet = new HttpGet(host + "/captcha?nocache=" + System.currentTimeMillis());
 
         HttpHost httpHost = new HttpHost(simple_host, 80);
         HttpPost post = new HttpPost();
@@ -41,34 +51,33 @@ public class Main {
         try {
             CloseableHttpResponse response;
             String code;
-            Map<String, Object> rtnMap;
+
             do {
 
                 code = getOcrCode(closeableHttpClient, httpGet);
-                post.setURI(new URI("/m/publicquery/vio?hpzl=02&hphm1b=AG98T3&hphm=辽AG98T3&fdjh=279903&captcha=" + code + "&qm=wf&page=1"));
+                URI uri = new URI(String.format("/m/publicquery/vio?hpzl=02&hphm1b=%s&hphm=%s&fdjh=%s&captcha=%s&qm=wf&page=1", hphm1b, hphm, fdjh, code));
+                post.setURI(uri);
                 response = closeableHttpClient.execute(httpHost, post, clientContext);
 
                 rtnMap = JSON.parseObject(response.getEntity().getContent(), Map.class);
             } while (rtnMap != null && (Integer) rtnMap.get("code") == 499);
-
-            System.out.println(JSON.toJSONString(rtnMap));
 
         } catch (IOException e) {
             e.printStackTrace();
         } catch (TesseractException e) {
             e.printStackTrace();
         }
+        return rtnMap;
     }
 
     private static String getOcrCode(CloseableHttpClient closeableHttpClient, HttpGet httpGet) throws IOException, TesseractException {
         CloseableHttpResponse response;
         String code;
         do {
-
-
             response = closeableHttpClient.execute(httpGet);
             BufferedImage img = ImageIO.read(response.getEntity().getContent());
             Tesseract tesseract = new Tesseract();
+            //path ./tessdata/eng.traineddata
             tesseract.setLanguage("eng");
             code = tesseract.doOCR(img);
 
@@ -87,9 +96,14 @@ public class Main {
      * @return true为包含，false为不包含
      */
     public static boolean isSpecialChar(String str) {
-        String regEx = "[ _`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]|\n|\r|\t";
-        Pattern p = Pattern.compile(regEx);
-        Matcher m = p.matcher(str);
-        return m.find();
+        boolean flag = false;
+        int[] charInts = str.chars().toArray();
+        for (int c : charInts) {
+            if (!(c > 47 && c < 123)) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
     }
 }
